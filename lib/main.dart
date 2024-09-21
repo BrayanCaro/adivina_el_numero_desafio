babc98f14fe0593c857c047e84b46389a001b5fe
Brayan Martínez Santana <brayanmtz@ciencias.unam.mx>
2024-09-20 22:05:05 -0600

    fix: Agrega color para resultados

--- lib/main.dart
+++ lib/main.dart
@@ -85,6 +85,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    var theme = Theme.of(context);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -133,8 +134,12 @@ class _MyHomePageState extends State<MyHomePage> {
                 children: [
                   const Text('Historial'),
                   for (var result in _previousGamesResults)
-                    // TODO ver como cambiar el color
-                    Text('${result.numberGuessed}'),
+                    Text(
+                      '${result.numberGuessed}',
+                      style: theme.textTheme.bodyMedium!.copyWith(
+                        color: result.isWinner ? Colors.green : Colors.red,
+                      ),
+                    ),
                 ],
               ),
             ),
